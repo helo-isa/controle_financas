6b182885b2cd6bd1d1668018ac4edcdf26e98207
Filipe Hollmann <116088308+FilipeGH03@users.noreply.github.com>
2024-09-23 11:05:05 -0300

    loginBack

--- routes/login.py
+++ routes/login.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from database.db import users
+
 
 login_route = Blueprint("login", __name__)
 
@@ -6,11 +8,26 @@ login_route = Blueprint("login", __name__)
 def login():
     return render_template('login.html')
 
+
+@login_route.route('/autenticar', methods=['GET'])
+def autenticar():
+    usuario = request.args.get('usuario')
+    passw = request.args.get('passw')
+    return "Usuario {} e senha{}".format(usuario,passw)
+  
+
+
+
 @login_route.route('/', methods=['GET'])
-def check_login():
-    pass
-
-
+def check_login(user, passw):
+    print(user, passw)
+    if users[user]:
+        if user[user] == passw:
+            return render_template("login.html")
+        else:
+            return render_template("login.html")
+    else:
+        return render_template("login.html")
 @login_route.route('/new', methods=['GET'])
 def register():
     pass
