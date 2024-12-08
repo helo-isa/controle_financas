e0fa73ad50d1b75170f8b397d356857bbb8828d9
Filipe Hollmann <116088308+FilipeGH03@users.noreply.github.com>
2024-12-08 19:14:17 -0300

    new

--- database/db.py
+++ database/db.py
@@ -3,57 +3,9 @@ users = {
 }
 
 ganhos = [
-{
-    "id" : 1,
-    "value": 50,
-    "date": "20/09/2024",
-    "class": "algo",
-},
-{
-    "id": 3,
-    "value": 100,
-    "date": "22/09/2024",
-    "class": "venda",
-},
-{
-    "id": 4,
-    "value": 150,
-    "date": "23/09/2024",
-    "class": "investimento",
-},
-{
-    "id": 5,
-    "value": 200,
-    "date": "24/09/2024",
-    "class": "bonus",
-}
 ]
 
 despesas = [
-    {
-        "id": 1,
-        "value": 50,
-        "date": "21/09/2024",
-        "class": "alimentação",
-    },
-    {
-        "id": 2,
-        "value": 100,
-        "date": "22/09/2024",
-        "class": "transporte",
-    },
-    {
-        "id": 3,
-        "value": 120,
-        "date": "23/09/2024",
-        "class": "moradia",
-    },
-    {
-        "id": 4,
-        "value": 45,
-        "date": "24/09/2024",
-        "class": "lazer",
-    },
 ]
 
 
